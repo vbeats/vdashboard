dcb463fe4f3e389f6a0b1c77f7569fc44e06a2f1
bootvue <bootvue@gmail.com>
2022-09-22 22:04:08 +0800

    remove test case

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # vdashboard
+
+## todo
+
+- [ ] doc
--- src/api/region/index.ts
+++
@@ -1,6 +0,0 @@
-import axios from '../../util/request'
-
-export function list(param?: any): Promise<any> {
-    return axios.post('/admin/region/list', {...param})
-}
-
